a644e43879b13bc6f7b64b964b1dd65e32b95f25
litmanhuang <litmanhuang@gmail.com>
2024-01-20 12:38:19 +0200

    refactor findChallengeResult

--- table.js
+++ table.js
@@ -39,6 +39,8 @@ const attendanceStored = localStorage.getItem('attendanceStoredLocal');
 const validatesStored2 = localStorage.getItem('challengeDatesStoredLocal2');
 const attendanceStored2 = localStorage.getItem('attendanceStoredLocal2');
 
+const challengeIds = [6205, 6204, 6206, 6207, 6208, 6209, 6210, 6211, 6212, 6213, 6214, 6215];
+
 // const testStored = localStorage.getItem('testStoredLocal');
 // console.log(testStored);
 // const testJSON = JSON.parse(testStored);
@@ -193,61 +195,6 @@ function populateProgressTable (data, tbody){
         tbody.innerHTML+=row
     }
 }
-
-//attendenceTable
-// function generateAttendenceTable(data){
-//     const heading = document.createElement("h1")
-//     heading.innerHTML = "Attendence Table"
-//     main.appendChild(heading)
-
-//     const attendenceTable = document.createElement("table")
-//     attendenceTable.id = "attendenceTable"
-//     attendenceTable.className="table table-striped"
-
-//     const tableHead = attendenceTable.createTHead();
-//     const headerRow = tableHead.insertRow();
-
-//     const headers = ["2023-09-21: 1500", "2023-09-28: 1500", "2023-10-05: 1500", "2023-10-12: 1500", "2023-10-19: 1500", "2023-10-26: 1500" ];
-//     headers.forEach(headerText => {
-//         const headerCell = document.createElement('th');
-//         headerCell.textContent = headerText;
-//         headerRow.appendChild(headerCell);
-//     });
-
-//     tableHead.appendChild(headerRow);
-//     attendenceTable.appendChild(tableHead)
-
-//     const tableBody = attendenceTable.createTBody();
-//     tableBody.id = "tableData"
-
-//     main.appendChild(attendenceTable)
-
-//     populateAttendenceTable(data, tableBody)
-// }
-
-// function populateAttendenceTable (data, tbody){
-//     for (let i = 0; i < data.length; i++) {
-//         let row =`<tr> 
-//                         <td> ${data[i].id}</td>
-//                         <td> ${data[i].firstName}</td>
-//                         <td> ${data[i].lastName}</td>
-//                         <td> ${data[i].email}</td>
-//                         <td> ${data[i].challenge1}</td>
-//                         <td> ${data[i].challenge2}</td>
-//                         <td> ${data[i].challenge3}</td>
-//                         <td> ${data[i].challenge4}</td>
-//                         <td> ${data[i].challenge5}</td>
-//                         <td> ${data[i].challenge6} </td>
-//                         <td> ${data[i].challenge7}</td>
-//                         <td> ${data[i].challenge8}</td>
-//                         <td> ${data[i].challenge9}</td>
-//                         <td> ${data[i].challenge10}</td>
-//                         <td> ${data[i].challenge11}</td>
-//                         <td> ${data[i].challenge12}</td>
-//                     </tr>`
-//         tbody.innerHTML+=row
-//     }
-// }
 
 function downloadFile(data, fileName, fileType) {
     // Create a Blob from the data
@@ -405,160 +352,31 @@ function attemptIsValid (accessDate, validDates, attendance){
 }
 
 // helper function to find challenges
-async function findChallengeResult (data, attendance, validDatesInput){
 
-    let challengeData = Array(12).fill("no attempt")
+async function findChallengeResult(data, attendance, validDatesInput, challengeIds) {
 
-    // console.log("starting challenge data" + challengeData)
+    let challengeData = Array(challengeIds.length).fill("no attempt");
+
     for (let i = 0; i < data.length; i++) {
+        let challenge = data[i].problemSubmissionAssignmentId;
+        let correct = data[i].problemSubmissionCorrect;
+        let accessDate = new Date(data[i].problemSubmissionTime);
 
-        let challenge = data[i].problemSubmissionAssignmentId
-        let correct = data[i].problemSubmissionCorrect
-        let accessDate = new Date (data[i].problemSubmissionTime)
+        let index = challengeIds.indexOf(challenge);
 
-        switch (challenge) {
-            // challenge 1 id = 6205
-            case 6205:
-            // case 3001:
-                if (correct !== null && challengeData[0]=="no attempt" && dateIsValid(accessDate, validDatesInput, attendance)){
-                    //problem, need to check attendance before checking attempts
-                    challengeData[0] = 0;
-                }
-
-                if (correct && dateIsValid(accessDate, validDatesInput, attendance)){
-                    challengeData[0] = challengeData [0] + 1
-                }
-                break;
-
-            // challenge 2 id = 6204
-            case 6204:
-            // case 3002:
-            if (correct !== null && challengeData[1]=="no attempt" && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[1] = 0;
+        if (index !== -1) {
+            if (correct !== null && challengeData[index] === "no attempt" && dateIsValid(accessDate, validDatesInput, attendance)) {
+                challengeData[index] = 0;
             }
-                if (correct && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[1] = challengeData [1] + 1
-                }
-                break;
-            
-            // challenge 3 id = 6206
-            case 6206:
-            // case 3003:
-            if (correct !== null && challengeData[2]=="no attempt" && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[2] = 0;
+
+            if (correct && dateIsValid(accessDate, validDatesInput, attendance)) {
+                challengeData[index] = challengeData[index] + 1;
             }
-                if (correct && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[2] = challengeData [2] + 1
-                }
-                break;
-
-            // challenge 4 id = 6207
-            case 6207:
-            // case 3004:
-            if (correct !== null && challengeData[3]=="no attempt" && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[3] = 0;
-            }
-                if (correct && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[3] = challengeData [3] + 1
-                }
-                break;
-
-            // challenge 5 id = 6208
-            case 6208:
-            // case 3005:
-            if (correct !== null && challengeData[4]=="no attempt" && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[4] = 0;
-            }
-                if (correct && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[4] = challengeData [4] + 1
-                }
-                break;
-
-            // challenge 6 id = 6209
-            case 6209:
-            // case 3006:
-            if (correct !== null && challengeData[5]=="no attempt" && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[5] = 0;
-            }
-                if (correct && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[5] = challengeData [5] + 1
-                }
-                break;
-
-            // challenge 7 id = 6210
-            case 6210:
-            // case 3007:
-            if (correct !== null && challengeData[6]=="no attempt" && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[6] = 0;
-            }
-                if (correct && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[6] = challengeData [6] + 1
-                }
-                break;
-
-            // challenge 8 id = 6211
-            case 6211:
-            // case 3008:
-            if (correct !== null && challengeData[7]=="no attempt" && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[7] = 0;
-            }
-                if (correct && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[7] = challengeData [7] + 1
-                }
-                break;
-
-            // challenge 9 id = 6212
-            case 6212:
-            // case 3009:
-                if (correct !== null && challengeData[8]=="no attempt" && dateIsValid(accessDate, validDatesInput, attendance)){
-                    challengeData[8] = 0;
-                }
-                if (correct && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[8] = challengeData [8] + 1
-                }
-                break;
-
-            // challenge 10 id = 6213
-            case 6213:
-            // case 3010:
-            if (correct !== null && challengeData[9]=="no attempt" && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[9] = 0;
-            }
-                if (correct && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[9] = challengeData [9] + 1
-                }
-                break;
-
-            // challenge 11 id = 6214 
-            case 6214:
-            // case 3011:
-
-            if (correct !== null && challengeData[10]=="no attempt" && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[10] = 0;
-            }
-                if (correct && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[10] = challengeData [10] + 1
-                }
-                break;
-
-            // challenge 12 id = 6215
-            case 6215:
-            // case 3012:
-            if (correct !== null && challengeData[11]=="no attempt" && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[11] = 0;
-            }
-                if (correct && dateIsValid(accessDate, validDatesInput, attendance)){
-                challengeData[11] = challengeData [11] + 1
-                }
-                break;
-            default:
-                break;
         }
     }
 
     challengeData = challengeData.map(number => number.toString());
-    // console.log(challengeData)
-    return challengeData
+    return challengeData;
 }
 
 async function catchCrime(id, firstName, lastName, assignments, validDates){
@@ -831,7 +649,7 @@ async function createStudentProgress(courseInput, attendanceInput, validDatesInp
 
             let studentCrimeReport = await catchCrime (studentCarnapData.id, studentCarnapData.firstName, studentCarnapData.lastName, assignmentData, validDatesInputParsed);
             crimeReports.push(studentCrimeReport);
-            let challengeData = await findChallengeResult(assignmentData, attendance, validDatesInputParsed);
+            let challengeData = await findChallengeResult(assignmentData, attendance, validDatesInputParsed, challengeIds);
 
             let student = new studentCarnap(studentCarnapData.id, studentCarnapData.email, studentCarnapData.firstName, studentCarnapData.lastName, challengeData[0], challengeData[1],challengeData[2], challengeData[3], challengeData[4], challengeData[5], challengeData[6], challengeData[7], challengeData[8], challengeData[9], challengeData[10], challengeData[11], attendance);
 
@@ -845,7 +663,7 @@ async function createStudentProgress(courseInput, attendanceInput, validDatesInp
         generateAndPopulateAttendanceTable(studentCarnapList);
         generateAndPopulateCrimeReportTable(crimeReports);
     } catch (error) {
-        console.error('Error creating student list:', error);
+        console.error('Error creating student table:', error);
     }
 };
 
